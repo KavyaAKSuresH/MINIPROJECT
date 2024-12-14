8b0f393466a8c543e2bd6892518afc9dc932f405
KavyaAKSuresH <kavyaksuresh2023@gmail.com>
2024-12-14 10:00:43 +0530

    i create faculty dash board

--- mycomplaint/base/urls.py
+++ mycomplaint/base/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
     path('register_complaint', views.register_complaint, name='register'),
     path('track_complaint', views.track_complaint, name='track_complaint'),
     path('view_complaint', views.view_complaint, name='view_complaint'),
+
+    path('facultyhome', views.facultyhome, name='facultyhome'),
+    path('notifications', views.notifications, name='notifications'),
+    path('new_complaints', views.new_complaints, name='new_complaints'),
+    path('pending_complaints', views.pending_complaints, name='pending_complaints'),
+    path('resolved_complaints', views.resolved_complaints, name='resolved_complaints'),
+    path('profile', views.profile, name='profile'),
+    path('logout', views.logout_view, name='logout'),
 ]
--- mycomplaint/base/views.py
+++ mycomplaint/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
@@ -12,4 +13,27 @@ def track_complaint(request):
 
 def view_complaint(request):
     return render(request, 'viewcomplaint.html')
+
+
+
+# Dashboard views
+def facultyhome(request):
+    return render(request, 'facultydash.html')
+def notifications(request):
+    return render(request, 'notifications.html')
+
+def new_complaints(request):
+    return render(request, 'new_complaints.html')
+
+def pending_complaints(request):
+    return render(request, 'pending_complaints.html')
+
+def resolved_complaints(request):
+    return render(request, 'resolved_complaints.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def logout_view(request):
+    return HttpResponse("You have been logged out.")
 
